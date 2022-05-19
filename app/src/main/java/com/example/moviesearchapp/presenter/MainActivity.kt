7ff3943a0ae0333d92f1f34efef4ebde155c002e
blucky8649 <blucky4511@gmail.com>
2022-05-19 14:38:54 +0900

    FEAT: 프래그먼트 생성

--- app/src/main/java/com/example/moviesearchapp/presenter/MainActivity.kt
+++ app/src/main/java/com/example/moviesearchapp/presenter/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviesearchapp
+package com.example.moviesearchapp.presenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviesearchapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/moviesearchapp/presenter/MovieFavoriteFragment/MovieFavoriteFragment.kt
@@ -0,0 +1,26 @@
+package com.example.moviesearchapp.presenter.MovieFavoriteFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moviesearchapp.databinding.FragmentMovieListBinding
+
+class MovieFavoriteFragment: Fragment() {
+    private var _binding: FragmentMovieListBinding? = null
+    private val binding = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMovieListBinding.inflate(layoutInflater, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
